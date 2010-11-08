1df2743df865325cd490445d6371abe0a633da5a
Mike Strauch <mike.strauch@hannonhill.com>
2010-11-07 22:26:42 -0500

    Began working on mapping rays to the screen and generating rays and crap.  Almost to the intersection part.

--- tracer.js
+++ tracer.js
@@ -7,6 +7,14 @@ function Color(r, g, b)
     this.r = r;
     this.g = g;
     this.b = b;
+	
+	/**
+	 * Returns a String that is used to set the color of the canvas context
+	 */
+	asCanvasRGB = function()
+	{
+		return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+	}
 }
 
 /**
@@ -70,8 +78,20 @@ function RayTraceableShape()
     this.color;
 }
 
+/**
+ * 
+ * @param {Object} viewPoint the point from which rays originate
+ * @param {Object} xRange x range in cartesian "units", this is treated as a +/- value, so a value of 5 would mean x goes from -5 to 5
+ * @param {Object} yRange y range in cartesian "units", this is treated as a +/- value, so a value of 5 would mean y goes from -5 to 5
+ * @param {Object} objectsInScene
+ * @param {Object} lights
+ * @param {Object} canvasElementId id of the canvas element in the page on which to draw
+ */
 function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElementId)
+
 {
+	this.backgroundColor = 'rgb(0, 0, 0)';
+	
     // the viewing point, or the point from which the scene will be drawn
     this.viewPoint;
     
@@ -103,17 +123,36 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
 		var context = canvas.getContext("2d");
         context.fillStyle = "#FF0000";
         
+		// the amount each pixel changes in x and y value in cartesian space
         var xIncrement = canvas.width / this.xRange;
         var yIncrement = canvas.height / this.yRange;
+		
+		// the x and y values for where the ray intersects the "screen", 
+		// initialized to point to the top left corner of the screen
+		var cartXValue = -xRange;
+		var cartYValue = yRange;
+		// the ray currently being processed
+		var currentRay;
+		
+		// the color to give the current pixel
+		var pixelColor;
         
         try 
 		{
-			// we're just drawing a rectangle for now to get this to work
-			for (i = 0; i < canvas.height; i++) 
+			// we're just drawing a rectangle for now to get this to draw something
+			for (y = 0; y < canvas.height; y++, cartYValue += yIncrement) 
 			{
-				for (j = 0; j < canvas.width; j++) 
-				{
-					context.fillRect(j, i, 1, 1);
+				for (x = 0; x < canvas.width; x++, cartXValue += xIncrement) 
+				{					
+					currentRay = generateRay(cartXValue, cartYValue);	
+					pixelColor = intersectRayWithScene(currentRay);	
+					
+					if(pixelColor != null)
+						context.fillStyle = pixelColor.asCanvasRGB();
+					else
+						contextFillStyle = backgroundColor;
+						
+					context.fillRect(y, x, 1, 1);
 				}
 			}
 		}
@@ -122,6 +161,29 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
 			alert(e);
 		}
     }
+	
+	intersectRayWithScene = function(ray)	
+	{
+		
+	}
+	
+	/**
+	 * Generates a new ray starting at the view point supplied on initialization and
+	 * ending at the point specified by x and y parameters (uses 0 for z value for now).
+	 * 
+	 * TODO: will probably need a more generic method to generate reflection/light rays and what not
+	 * 
+	 * @param {Object} x
+	 * @param {Object} y
+	 */
+	generateScreenRay = function(x, y)
+	{
+		var screenPoint = new Point(x, y, 0); // z value is 0 because we've strategically placed the screen right on the 0 of z axis
+		                                      // can update this later
+		var result = new Ray(this.viewPoint, screenPoint);
+		result.normalize();
+		return result;
+	}
 }
 
 /**
