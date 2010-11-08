247ea26267e6ea1612b8d5765fb762162ab4e2a0
Mike Strauch <mike.strauch@hannonhill.com>
2010-11-08 01:48:28 -0500

    Started adding math for intersection of ray and sphere.  Added some util methods for calculating dot product and such.  Linear algebra is slowly coming back to me.

--- tracer.js
+++ tracer.js
@@ -25,6 +25,15 @@ function Point(x, y, z)
     this.x = x;
     this.y = y;
     this.z = z;
+	
+	this.minus = function(p)
+	{
+		var result = new Point();
+		result.x = this.x - p.x;
+		result.y = this.y - p.y;
+		result.z = this.z - p.z;
+		return result;
+	}
 }
 
 
@@ -53,14 +62,70 @@ function Ray(startingPoint, directionPoint)
 function Shape()
 {
     /**
-     * Determines where a Ray intersects with the shape.
+     * Gets the intersection points of a Ray on this particular Shape.
      * Returns an array of Points (the ray could intersect with the shape more than once)
      * or null if the Ray does not intersect the shape.
      */
-    this.rayIntersects = function(ray)
-    {
+    this.getIntersections = function(ray)
     
+	{
+		var result = new Array();
+		result.push(new Point(1, 1, 1));
+		return result;
     }
+}
+
+/**
+ * A shape that occurs in a ray-traceable scene.
+ * Has a color so that it can be drawn on a canvas.
+ */
+function RayTraceableShape(color)
+{
+    this.color = color;
+}
+
+Sphere.prototype = new RayTraceableShape();
+Sphere.prototype.constructor = Sphere;
+function Sphere(centerPoint, radius, color)
+{
+	RayTraceableShape.constructor.call(this, color);
+	this.centerPoint = centerPoint;
+	this.radius = radius;
+}
+
+/**
+ * Determines the intersection points between a ray and a sphere.
+ * @param {Object} ray
+ */
+Sphere.prototype.getIntersections = function(ray)
+{
+	// TODO: check this math, what's linear algebra again? what's algebra again?
+	var result = new Array();
+	var a = dotProduct(ray.directionPoint, ray.directionPoint);
+	
+	var rayOriginMinusCenter = ray.startingPoint.minus(this.centerPoint);
+	var b = 2 * dotProduct(rayOriginMinusCenter, ray.directionPoint);
+	var c = dot(rayOriginMinusCenter, rayOriginMinusCenter) - Math.pow(this.radius, 2);
+	
+	try 
+	{	
+		var sqrtValue = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+		var value1 = (-b + sqrtValue) / 2 * a;
+		var value2 = (-b - sqrtValue) / 2 * a;
+		
+		point1 = new Point(ray.directionPoint.x * value1, ray.directionPoint.y * value1, ray.directionPoint.z * value1);
+		point2 = new Point(ray.directionPoint.x * value2, ray.directionPoint.y * value2, ray.directionPoint.z * value2);
+		
+		result.push(point1);
+		result.push(point2);
+	}
+	// we can run into issues if there are no roots for the equation, i'm not exactly sure what will happen if there are no roots
+	// probably run into imaginary numbers when trying to take sqrt of a negative	
+	catch(e)
+	{
+		alert("No roots found!");
+	}
+	return result;
 }
 
 function LightSource(location)
@@ -68,15 +133,7 @@ function LightSource(location)
 	this.location = location;
 }
 
-/**
- * A shape that occurs in a ray-traceable scene.
- * Has a color so that it can be drawn on a canvas.
- */
-function RayTraceableShape()
-{
-    this.shape;
-    this.color;
-}
+
 
 /**
  * 
@@ -139,9 +196,12 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
         
         try 
 		{
-			// we're just drawing a rectangle for now to get this to draw something
+			// iterate down the canvas
 			for (y = 0; y < canvas.height; y++, cartYValue += yIncrement) 
 			{
+				// iterate across the canvas and fill in pixels with object
+				// colors or the background color depending on whether or not
+				// the generated ray intersects with any objects				
 				for (x = 0; x < canvas.width; x++, cartXValue += xIncrement) 
 				{					
 					currentRay = generateRay(cartXValue, cartYValue);	
@@ -162,13 +222,44 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
 		}
     }
 	
+	/**
+	 * For now, just finds the closest object in the scene that intersects with a ray
+	 * and returns its color.
+	 * 
+	 * @param {Object} ray the ray to intersect with the scene
+	 */
 	intersectRayWithScene = function(ray)	
 	{
+		// keep track of the object closest to the view point
+		var closestObject = null;
+		// also keep track of the shortest intersection distance
+		var closestDistance = null;
 		
+		for(i = 0; i < objectsInScene.length; i++)
+		{	
+			// points of intersection (could be more than 1 depending on the shape
+			intersectionPoints = objectsInScene[i].getIntersections(ray);
+			if(intersectionPoints != null)
+			{
+				for(j = 0; j < intersectionPoints.length; j++)
+				{
+					var intersectionPoint = intersectionPoints[j];
+					var distance = getDistance(intersectionPoint, ray.startingPoint);
+					
+					if (closestDistance == null || (closestDistance != null && distance < closestDistance)) 
+					{
+						closestDistance = distance;
+						closestObject = objectsInScene[i];
+					}
+				}
+			}
+		}
+		
+		return closestObject == null ? null : closestObject.color;
 	}
 	
 	/**
-	 * Generates a new ray starting at the view point supplied on initialization and
+	 * Generates a new ray starting at the view point supplied during initialization and
 	 * ending at the point specified by x and y parameters (uses 0 for z value for now).
 	 * 
 	 * TODO: will probably need a more generic method to generate reflection/light rays and what not
@@ -178,7 +269,7 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
 	 */
 	generateScreenRay = function(x, y)
 	{
-		var screenPoint = new Point(x, y, 0); // z value is 0 because we've strategically placed the screen right on the 0 of z axis
+		var screenPoint = new Point(x, y, 0); // z value is 0 because we've strategically placed the screen on the 0 of z axis
 		                                      // can update this later
 		var result = new Ray(this.viewPoint, screenPoint);
 		result.normalize();
@@ -191,7 +282,7 @@ function Tracer(viewPoint, xRange, yRange, objectsInScene, lights, canvasElement
  * @param {Object} point1
  * @param {Object} point2
  */
-function distance(point1, point2)
+function getDistance(point1, point2)
 {
     var squareDiffX = differenceSquare(point1.x, point2.x);
     var squareDiffY = differenceSquare(point1.y, point2.y);
@@ -208,4 +299,13 @@ function distance(point1, point2)
 function differenceSquare(a, b)
 {
     return Math.pow(point1.x - point2.x, 2);
+}
+
+function dotProduct(v1, v2)
+{
+	var total;
+	total += v1.x * v2.x;
+	total += v1.y * v2.y;
+	total += v1.z * v2.z;
+	return total;
 }
